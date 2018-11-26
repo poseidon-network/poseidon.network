6e94a7c6521c120864192995f7eaad50830f7616
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-26 18:55:47 +0800

    Add data

---
+++ data.ts
@@ -0,0 +1,135 @@
+export const header = {
+  description: `World’s First\nDecentralized Content\nAcceleration Network\n`,
+};
+
+export const indicators = [
+  {
+    value: 500,
+    unit: '+',
+    description: 'Active nodes',
+  },
+  {
+    value: 20,
+    unit: 'Cites',
+    description: 'Around the world',
+  },
+  {
+    value: 2700,
+    unit: 'TB',
+    description: 'Bandwith delivered',
+  },
+  {
+    value: 400,
+    unit: 'TB',
+    description: 'Storage',
+  },
+];
+
+export const intro = [
+  {
+    title: 'Vision',
+    description: 'Build a Sharing Economy for decentralized web with the world’s unused bandwidth, storage, and computation.',
+  },
+  {
+    title: 'What is\nPOSEIDON NETWORK',
+    description: 'Next-Generation Content Acceleration Layer (CDN + DSN), incentivized by Token Economy, it’s cheaper yet faster.\nUtilized unused bandwidth and storage from any NAS, desktop, or mobile devices around the world. It’s distributed, efficient, and integrates perfectly with existing internet and blockchain infrastructure\nServed as a bandwidth optimization mechanism for every type of web content (static file / video on demand / live streaming)',
+  },
+];
+
+export const features = [
+  {
+    imageUri: '/static/img-1.png',
+    title: 'Build Sharing  Economy',
+    description: 'Utilized unused bandwidth and storage\nfrom any NAS, desktop, or mobile\ndevices around the world. It’s distributed, efficient, and integrates perfectly with existing internet and blockchain infrastructure',
+  },
+  {
+    imageUri: '/static/img-2.png',
+    title: 'Faster',
+    description: 'Benefit from Fog Computing\ntechnology.',
+  },
+  {
+    imageUri: '/static/img-3.png',
+    title: 'Cheaper',
+    description: 'With Fog Computing technology. We provide a cheaper content\nacceleration Layer incentivized by PSD Token.  “$0.01 USD/GB, almost ten times cheaper than AWS cloudfront”.',
+  },
+  {
+    imageUri: '/static/img-4.png',
+    title: 'Production Ready',
+    description: 'Real customers delivering\nand storing content.',
+  },
+  {
+    imageUri: '/static/img-5.png',
+    title: 'Layer 2 Solution',
+    description: 'NOT putting data on blockchain to ensure\nperformance,\nscalability, and feasibility. ',
+  },
+  {
+    imageUri: '/static/img-6.png',
+    title: 'Multipurpose',
+    description: 'Data storage, bandwidth,\nand computation.',
+  },
+]
+
+export const partners = [
+  { logoUri: '/static/img-aba.png', alt: 'ABA' },
+  { logoUri: '/static/group-4.png', alt: 'ACE' },
+  { logoUri: '/static/img-nem.png', alt: 'nem' },
+  { logoUri: '/static/img-gcp.png', alt: 'GCP' },
+  { logoUri: '/static/img-joyso.png', alt: 'JOYSO' },
+  { logoUri: '/static/img-longhash.png', alt: 'LONGHASH' },
+];
+
+export const roadmap = [
+  {
+    time: 'WHERE WE START',
+    description: 'Bootstrapping Qinfra from 2 co-founders Qing & Mike, target to disrupt cloud infra with innovative Edge Computing Technology',
+    done: true,
+  },
+  {
+    time: 'Q2 - Q3 2018',
+    description: 'Core Development',
+    done: true,
+  },
+  {
+    time: 'Q4 2018',
+    title: 'EQUITY Round',
+    description: 'Centralized dispatcher\nDistributed Edges',
+    active: true,
+  },
+  {
+    time: 'Q1 2019',
+    title: 'Public Sale on ACE',
+    description: 'Distributed Qontroller\nDistributed Edges',
+    lastone: true,
+  },
+];
+
+export const team = [
+  { name: 'Light Lin', title: 'CEO', avatar: '' },
+  { name: 'Milke Lin', title: 'CEO', avatar: '' },
+];
+
+export const techCoreTeam = [
+  { name: 'Light Lin', title: 'CEO', avatar: '' },
+  { name: 'Milke Lin', title: 'CEO', avatar: '' },
+];
+
+export const advisors = [
+  { name: 'Light Lin', title: 'CEO', avatar: '' },
+  { name: 'Milke Lin', title: 'CEO', avatar: '' },
+];
+
+export const contact = {
+  title: 'Follow Our Journey',
+  description: 'Bootstrapping Qinfra from 2 co-founders Qing & Mike, target to disrupt cloud infra …',
+};
+
+export const footer = {
+  socialMediaList: [
+    { imgUri: '/static/ic-telegram.png', link: '/', alt: 'Telegram' },
+    { imgUri: '/static/ic-twitter.png', link: '/', alt: 'Twitter' },
+    { imgUri: '/static/ic-medium.png', link: '/', alt: 'Medium' },
+  ],
+  email: 'hi@poseidon.network',
+  copyright: 'POSEIDON NETWORK © 2018 All rights reserved.',
+};
+
