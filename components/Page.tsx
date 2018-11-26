8e4b38863079d9d88b4c375d1619de9bef453079
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-26 23:29:46 +0800

    Add favicon.ico

--- components/Page.tsx
+++ components/Page.tsx
@@ -6,6 +6,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => ((
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
       <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+      <link rel="icon" type="image/png" sizes="32x32" href="./static/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="./static/favicon-16x16.png" />
     </Head>
     { children }
 
