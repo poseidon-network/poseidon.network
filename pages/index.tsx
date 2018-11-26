cfaa54e10e34b7371cfefeb4c1e1da82d2fd4e66
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-26 18:55:26 +0800

    Add page

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+
+import Page from '../components/Page';
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import Indicators from '../components/Indicators';
+import Intro from '../components/Intro';
+import Features from '../components/Features';
+import Partners from '../components/Partners';
+import Roadmap from '../components/Roadmap';
+import Team from '../components/Team';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+
+const Index = () => ((
+  <Page>
+    <Head>
+      <title>Poseidon Network</title>
+    </Head>
+    <Nav />
+    <Header />
+    <Indicators />
+    <Intro />
+    <Features />
+    <Partners />
+    <Roadmap />
+    <Team />
+    <Contact />
+    <Footer />
+  </Page>
+));
+
+export default Index;
