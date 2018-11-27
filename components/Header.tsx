7b05ac3bbe65747fd1546c9de05bf84ee3c5bd8b
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-27 13:02:50 +0800

    Add bg to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,14 @@ import { header } from '../data';
 const Header = () => ((
   <Layout bgColor="#222633" color="#fff">
     <Content>
-      <p>
-        { header.description }
-      </p>
+      <div className="description-container">
+        <div className="bg-rect" />
+        <div className="bg-rect" />
+        <div className="bg-rect" />
+        <p>
+          { header.description }
+        </p>
+      </div>
       <img alt="world map" src="./static/img-map@3x.png" />
 
       <style jsx>{`
@@ -27,11 +32,47 @@ const Header = () => ((
           margin: auto;
         }
 
+        .description-container {
+          position: relative;
+        }
+
+        .bg-rect {
+          width: 311px;
+          height: 6px;
+          opacity: 0.3;
+          position: absolute;
+          background-image: linear-gradient(to right, rgba(170, 209, 204, 0), #aad1cc 52%, rgba(170, 209, 204, 0));
+        }
+
+        .bg-rect:nth-child(1) {
+          top: 37px;
+        }
+
+        .bg-rect:nth-child(2) {
+          top: 66px;
+        }
+
+        .bg-rect:nth-child(3) {
+          top: 95px;
+        }
+
         @media only screen and (min-width: 600px) {
           p {
             font-size: 26px;
             margin-bottom: 80px;
             line-height: 1.40;
+          }
+
+          .bg-rect:nth-child(1) {
+            top: 45px;
+          }
+  
+          .bg-rect:nth-child(2) {
+            top: 82px;
+          }
+  
+          .bg-rect:nth-child(3) {
+            top: 118px;
           }
         }
       `}</style>
