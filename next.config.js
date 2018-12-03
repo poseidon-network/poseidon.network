d34157349469973f8a18bf0ed6a320db0fbcef09
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-12-03 16:53:44 +0800

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const withTypescript = require('@zeit/next-typescript');
 
 module.exports = withTypescript({
@@ -5,4 +6,10 @@ module.exports = withTypescript({
     '/': { page: '/' },
     '/demo': { page: '/demo' },
   }),
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin({
+      IPFS_HOST: process.env.IPFS_HOST,
+    }));
+    return config;
+  },
 });
