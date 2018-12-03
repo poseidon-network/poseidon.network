1e73f99a5b2f644db569d3d9b70f6fe74de5592e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-12-03 13:29:44 +0800

    Create demo

---
+++ components/Demo.tsx
@@ -0,0 +1,13 @@
+import Layout from './Layout';
+import Content from './Content';
+import UploadFileForm from './UploadFileForm';
+
+const Demo = () => ((
+  <Layout bgColor="#d7f2ee" color="#222633">
+    <Content>
+      <UploadFileForm />
+    </Content>
+  </Layout>
+));
+
+export default Demo;
--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const withTypescript = require('@zeit/next-typescript');
 module.exports = withTypescript({
   exportPathMap: () => ({
     '/': { page: '/' },
+    '/demo': { page: '/demo' },
   }),
 });
