5d569a8732e4f44e645642956b11ae6f33d660ae
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-12-07 00:27:55 +0800

    Lazy load

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,82 +1,107 @@
+import React from 'react';
 import Layout from './Layout';
 import Content from './Content';
 import { header } from '../data';
 
-const Header = () => ((
-  <Layout bgColor="#222633" color="#fff">
-    <Content>
-      <div className="description-container">
-        <div className="bg-rect" />
-        <div className="bg-rect" />
-        <div className="bg-rect" />
-        <p>
-          { header.description }
-        </p>
-      </div>
-      <img className="map" src="/static/map.gif" />
-      <style jsx>{`
-        p {
-          text-align: center;
-          font-size: 22px;
-          font-weight: bold;
-          line-height: 1.36;
-          height: 90px;
-          white-space: pre-line;
-        }
+const loadImage = (src: string) => (
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => resolve(src);
+    image.onerror = err => reject(err);
+    image.src = src;
+  })
+);
 
-        .map {
-          width: 100%;
-          margin-bottom: 0px;
-          height: 100%;
-          max-width: 920px;
-        }
+class Header extends React.Component {
+  state = {
+    mapSrc: '/static/img-map@3x.png',
+  };
 
-        .description-container {
-          position: relative;
-        }
+  async componentDidMount() {
+    await loadImage('/static/map.gif');
+    this.setState({
+      mapSrc: '/static/map.gif',
+    });
+  }
 
-        .bg-rect {
-          width: 311px;
-          height: 6px;
-          opacity: 0.3;
-          position: absolute;
-          background-image: linear-gradient(to right, rgba(170, 209, 204, 0), #aad1cc 52%, rgba(170, 209, 204, 0));
-        }
+  render() {
+    return (
+      <Layout bgColor="#222633" color="#fff">
+        <Content>
+          <div className="description-container">
+            <div className="bg-rect" />
+            <div className="bg-rect" />
+            <div className="bg-rect" />
+            <p>
+              { header.description }
+            </p>
+          </div>
+          <img alt="our nodes in the world map" className="map" src={this.state.mapSrc} />
+          <style jsx>{`
+            p {
+              text-align: center;
+              font-size: 22px;
+              font-weight: bold;
+              line-height: 1.36;
+              height: 90px;
+              white-space: pre-line;
+            }
 
-        .bg-rect:nth-child(1) {
-          top: 37px;
-        }
+            .map {
+              width: 100%;
+              margin-bottom: 0px;
+              height: 100%;
+              max-width: 920px;
+            }
 
-        .bg-rect:nth-child(2) {
-          top: 66px;
-        }
+            .description-container {
+              position: relative;
+            }
 
-        .bg-rect:nth-child(3) {
-          top: 95px;
-        }
+            .bg-rect {
+              width: 311px;
+              height: 6px;
+              opacity: 0.3;
+              position: absolute;
+              background-image: linear-gradient(to right, rgba(170, 209, 204, 0), #aad1cc 52%, rgba(170, 209, 204, 0));
+            }
 
-        @media only screen and (min-width: 600px) {
-          p {
-            font-size: 26px;
-            margin-bottom: 80px;
-            line-height: 1.40;
-          }
+            .bg-rect:nth-child(1) {
+              top: 37px;
+            }
 
-          .bg-rect:nth-child(1) {
-            top: 45px;
-          }
+            .bg-rect:nth-child(2) {
+              top: 66px;
+            }
 
-          .bg-rect:nth-child(2) {
-            top: 82px;
-          }
+            .bg-rect:nth-child(3) {
+              top: 95px;
+            }
 
-          .bg-rect:nth-child(3) {
-            top: 118px;
-          }
-        }
-      `}</style>
-    </Content>
-  </Layout>
-));
+            @media only screen and (min-width: 600px) {
+              p {
+                font-size: 26px;
+                margin-bottom: 80px;
+                line-height: 1.40;
+              }
+
+              .bg-rect:nth-child(1) {
+                top: 45px;
+              }
+
+              .bg-rect:nth-child(2) {
+                top: 82px;
+              }
+
+              .bg-rect:nth-child(3) {
+                top: 118px;
+              }
+            }
+          `}</style>
+        </Content>
+      </Layout>
+    );
+  }
+}
 
 export default Header;
