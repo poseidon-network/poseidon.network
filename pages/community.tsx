a7340bff1de92ee49949e81d90913773f0216b5b
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-05-09 23:47:41 +0800

    Add community

---
+++ pages/community.tsx
@@ -0,0 +1,19 @@
+import Page from '../layout/Page';
+import Contact from '../components/Contact';
+import Header from '../components/community/Header';
+import Miner from '../components/community/Miner';
+import Embassador from '../components/community/Embassador';
+import Blog from '../components/community/Blog';
+import { styles } from '../constants';
+
+const Community = () => ((
+  <Page title="Poseidon Network" navColor={styles.dark}>
+    <Header />
+    <Miner />
+    <Embassador />
+    <Blog />
+    <Contact />
+  </Page>
+));
+
+export default Community;
