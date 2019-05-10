f7bb8a1ceb8964a141ec239b998c8e81f2314062
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-05-10 19:03:20 +0800

    Add export path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const withTypescript = require('@zeit/next-typescript');
 module.exports = withTypescript({
   exportPathMap: () => ({
     '/': { page: '/' },
+    '/technology': { page: '/technology' },
+    '/company': { page: '/company' },
+    '/token': { page: '/token' },
+    '/community': { page: '/community' },
     '/demo': { page: '/demo' },
     '/preview': { page: '/preview', query: { q: '' } },
     '/tron-dapp': { page: '/tron-dapp' },
