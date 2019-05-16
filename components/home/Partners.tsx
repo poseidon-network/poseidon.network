7237948f17680129a262f541228ec0cb2f30cced
Chi-Hsuan Huang <chihsuan.huang.7@gmail.com>
2019-05-16 20:22:33 +0800

    Improve home page (#11)
    * Improve style
    
    * Fix community header
    
    * Update home page
    
    * Fix tsc check
    
    * Fix home ui
    
    * Update style
    
    * Fix margin
    
    * Update footer link
    
    * Update page style
    
    * Fix export content empty
    
    * Fix social media list style
    
    * Fix for small screen
    
    * Fix media list style
    
    * Fix small screen style
    
    * Add prettier
    
    * Improve home page style

--- components/home/Partners.tsx
+++ components/home/Partners.tsx
@@ -6,48 +6,48 @@ import Content from '../Content';
 import { styles } from '../../constants';
 import { partners } from '../../data';
 
-const Partners = () => ((
+const Partners = () => (
   <Section padding="110px 0 120px" bgColor={styles.dark} color="#fff">
     <Content>
-      <H2 id="partners" margin="0 0 60px" size="32px" center>Our Strong Partners</H2>
+      <H2 id="partners" margin="0 0 60px" size="32px" center>
+        Our Strong Partners
+      </H2>
       <div className="partners">
-        {
-          partners.map(({ name, title, avatar }) => ((
-            <Person
-              key={name}
-              name={name}
-              title={title}
-              avatar={avatar}
-              backgroundColor="#000"
-            />
-          )))
-        }
+        {partners.map(({ name, title, avatar }) => (
+          <Person
+            key={name}
+            name={name}
+            title={title}
+            avatar={avatar}
+            backgroundColor="#000"
+          />
+        ))}
       </div>
 
       <style jsx>{`
         .partners {
           width: 100%;
           display: grid;
-          grid-template-columns: 160px 160px 160px 160px;
+          grid-template-columns: auto auto auto auto;
+          grid-gap: 0 30px;
           justify-content: space-around;
         }
 
         @media only screen and (max-width: 768px) {
           .partners {
-            grid-template-columns: 160px 160px;
             grid-gap: 60px 0;
           }
         }
 
         @media only screen and (max-width: 554px) {
           .partners {
-            grid-template-columns: 141px 141px;
+            grid-template-columns: auto auto;
             grid-gap: 10px 34px;
           }
         }
       `}</style>
     </Content>
   </Section>
-));
+);
 
 export default Partners;
