68155853fd2d444db5cdb3db8f278f638ed43918
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-07-01 13:57:48 +0800

    Add privacy page

--- next.config.js
+++ next.config.js
@@ -9,20 +9,24 @@ module.exports = withTypescript({
     '/token': { page: '/token' },
     '/community': { page: '/community' },
     '/demo': { page: '/demo' },
+    '/privacy': { page: '/privacy' },
     '/preview': { page: '/preview', query: { q: '' } },
     '/tron-dapp': { page: '/tron-dapp' },
   }),
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin({
-      GRAPHQL_URI: process.env.GRAPHQL_URI || 'http://localhost:4000/graphql',
-      FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '',
-      CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS | '',
-    }));
+    config.plugins.push(
+      new webpack.EnvironmentPlugin({
+        GRAPHQL_URI: process.env.GRAPHQL_URI || 'http://localhost:4000/graphql',
+        FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '',
+        CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS | '',
+      }),
+    );
     return config;
   },
   publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
+    localeSubpaths:
+      typeof process.env.LOCALE_SUBPATHS === 'string'
+        ? process.env.LOCALE_SUBPATHS
+        : 'none',
   },
 });
