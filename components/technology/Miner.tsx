f379cd8ba66f7219e0aeae5d40e7e15b5ca1d3cb
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-08-03 20:17:39 +0900

    Fix Miner section

--- components/technology/Miner.tsx
+++ components/technology/Miner.tsx
@@ -3,7 +3,6 @@ import Content from '../Content';
 import P from '../P';
 import Col from '../Col';
 import H2 from '../H2';
-import RParallax from '../RParallax';
 // import Button from '../Button';
 
 import { withTranslation } from '../../i18n';
@@ -16,46 +15,44 @@ const Miner = ({ t }: ITrans) => {
       color={styles.primaryColor}
       padding="120px"
     >
-      <RParallax percentage={0.5} speed={-13}>
-        <Content direction="row">
-          <Col flex style="algin-items: center;">
-            <img src="/static/tech-miner.svg" />
-          </Col>
-          <Col flex style="justify-content: center;">
-            <H2>{t('tech.miner')}</H2>
-            <P mStyle="text-align: left;">{t('tech.miner.description')}</P>
-          </Col>
-        </Content>
-        {/* <Content direction="row" padding="190px 0 0 0">
-        <Col style="margin-right: 40px">
-          <Button title="Mobile App" uri=""/>
+      <Content direction="row">
+        <Col flex style="algin-items: center;">
+          <img src="/static/tech-miner.svg" />
         </Col>
-        <Col style="margin-right: 40px">
-          <Button title="Desktop App" uri=""/>
+        <Col flex style="justify-content: center;">
+          <H2>{t('tech.miner')}</H2>
+          <P mStyle="text-align: left;">{t('tech.miner.description')}</P>
         </Col>
-        <Col>
-          <Button title="NAS" uri=""/>
-        </Col>
-      </Content> */}
-        <style jsx>{`
-          h2 {
-            height: 80px;
-            font-family: Montserrat;
-            font-size: 60px;
-            font-weight: 600;
-            line-height: 1.33;
-            letter-spacing: normal;
-            color: #d7f2ee;
-            margin-bottom: 60px;
-          }
+      </Content>
+      {/* <Content direction="row" padding="190px 0 0 0">
+      <Col style="margin-right: 40px">
+        <Button title="Mobile App" uri=""/>
+      </Col>
+      <Col style="margin-right: 40px">
+        <Button title="Desktop App" uri=""/>
+      </Col>
+      <Col>
+        <Button title="NAS" uri=""/>
+      </Col>
+    </Content> */}
+      <style jsx>{`
+        h2 {
+          height: 80px;
+          font-family: Montserrat;
+          font-size: 60px;
+          font-weight: 600;
+          line-height: 1.33;
+          letter-spacing: normal;
+          color: #d7f2ee;
+          margin-bottom: 60px;
+        }
 
-          @media only screen and (max-width: 1024px) {
-            img {
-              width: 100%;
-            }
+        @media only screen and (max-width: 1024px) {
+          img {
+            width: 100%;
           }
-        `}</style>
-      </RParallax>
+        }
+      `}</style>
     </Section>
   );
 };
